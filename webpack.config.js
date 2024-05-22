413f81edf98308b4726a7274161e1b8016ae9e0c
Bgoutham-krishna <165013635+Bgoutham-krishna@users.noreply.github.com>
2024-05-22 18:41:54 +1000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+   resolve: {
+    fallback: {
+      "zlib": require.resolve("browserify-zlib"),
+      "querystring": require.resolve("querystring-es3"),
+      "path": require.resolve("path-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "fs": false,
+      "http": require.resolve("stream-http"),
+      "stream": require.resolve("stream-browserify"),
+      "net": false,
+    },
+  },
   mode: 'production', // Or 'development' if that's what you need
   module: {
     rules: [
